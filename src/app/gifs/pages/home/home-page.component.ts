08d2c94aaf29721ab40bbcf4d945fb4a9fd1581f
Diego Ayala <namadian13@gmail.com>
2024-04-23 23:12:14 -0400

    Gifs-App

---
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'Gifs-home-page',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+
+  constructor( private GifsService: GifsService ) {}
+
+  get gifs(): Gif[] {
+    return this.GifsService.gifsList;
+  }
+
+}
